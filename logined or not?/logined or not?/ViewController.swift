dfd167c6897c8f2422bd4f7e24ab2ff264bbcfa5
lakshaychhabra <lakshay.chhabra@hotmail.com>
2018-02-04 01:49:08 +0530

    Core Module : Save , Update

--- logined or not?/logined or not?/ViewController.swift
+++ logined or not?/logined or not?/ViewController.swift
@@ -11,13 +11,52 @@ import CoreData
 
 class ViewController: UIViewController {
 
+    var isloogedIn = false
     
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var logoutButton: UIButton!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var loginButton: UIButton!
    
     
+    @IBAction func logout(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        let context = appDelegate?.persistentContainer.viewContext
+        
+          let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        do {
+            
+            let results = try context?.fetch(request)
+            
+            for result in results! {
+                
+                context?.delete(result as! NSManagedObject)
+                
+                do{
+                    try context?.save()
+                }
+                catch{
+                    print("Individual delete fail")
+                }
+                
+            }
+//            textField.alpha = 1
+            loginButton.setTitle("Log In", for: [])
+            label.alpha = 0
+            logoutButton.alpha = 0
+            isloogedIn = false
+            
+            
+        }
+        catch{
+            print("Delete Failed")
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,10 +76,11 @@ class ViewController: UIViewController {
             
             for result in (results as? [NSManagedObject])! {
                 if let username = result.value(forKey: "username") as? String {
-                    textField.alpha = 0
-                    loginButton.alpha = 0
+                    textField.alpha = 1
+                    loginButton.setTitle("Update Username", for: [])
                     label.alpha = 1
                     label.text  = "Hi There \(username) !"
+                    logoutButton.alpha = 1
                     
                 }
                 
@@ -68,22 +108,56 @@ class ViewController: UIViewController {
         let appDelegate = UIApplication.shared.delegate as? AppDelegate
         
         let context = appDelegate?.persistentContainer.viewContext
-        
-        let newUsers = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context!)
-        
-        newUsers.setValue(textField.text, forKey: "username")
-        
-        do{
-            try context?.save()
-            textField.alpha = 0
-            loginButton.alpha = 0
-            label.alpha = 1
-            label.text  = "Hi There \(textField.text!) !"
-            print("Saved")
+       
+        if isloogedIn {
+            
+             let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+            
+            do{
+                let results = try context?.fetch(request)
+                
+                if results!.count > 0 {
+                    
+                    for result in results as! [NSManagedObject]
+                    {
+                        result.setValue(textField.text, forKey: "username")
+                    
+                        do{
+                            try context?.save()
+                        }
+                        catch{
+                            print("Update username Failed")
+                        }
+                    }
+                label.text = "Hey there " + textField.text! + "!"
+                    
+             }
+            }
+            catch{
+                print("Update Failes")
+            }
+            
         }
-        catch{
-            print(error)
-        }
+        else{
+            let newUsers = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context!)
+            
+            newUsers.setValue(textField.text, forKey: "username")
+            
+            do{
+                try context?.save()
+                
+                loginButton.setTitle("Update Username", for: [])
+                label.alpha = 1
+                
+                label.text  = "Hi There \(textField.text!) !"
+                print("Saved")
+                isloogedIn = true
+                logoutButton.alpha = 1 
+            }
+            catch{
+                print("error failed to save")
+                }
+            }
     }
     
 }
